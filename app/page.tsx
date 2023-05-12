6b31766ec69635d8c44c8b7bb2eeb082e35e7001
dreamjunky <tucuxikay@gmail.com>
2023-05-12 18:22:23 -0500

    132123

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import React, { useState, useEffect } from "react";
 
 
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
