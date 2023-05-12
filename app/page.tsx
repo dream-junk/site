9bde3ee451c258a15da600076dbe6f7174084021
dreamjunky <tucuxikay@gmail.com>
2023-05-12 17:32:25 -0500

    init

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="flex items-center justify-center w-full">
+        <Image src="/dreamjunk.png" alt="Dream Junk" height="300" width="300" className="main-logo"/>
+      </div>
+    </main>
+  )
+}
